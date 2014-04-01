211fc5487194cfa7eceab399bbd1d2a74753fae8
Preston Maness <aggroskater@gmail.com>
2014-04-01 03:50:09 -0500

    I think I've nailed this.

--- Project-4-4bit-ALU/lib/shift_rotate/start.vhd
+++ Project-4-4bit-ALU/lib/shift_rotate/start.vhd
@@ -15,3 +15,28 @@
 --the two least significant bits are always the same!
 
 --Shifting on the other hand still looks a bit dicey.
+
+--Update a few days later:
+
+--I can't think of a good way to do this completely combinatorially.
+--I keep wanting to use flip-flops. However, we could still use the 
+--barrel shifter idea above to shift as well as rotate. We'd need to 
+--wire the result of ( B > 3 ) to the shifter though. If B > 3, AND 
+--opcode implies logical shift, clear out the result of the barrel 
+--shift. Otherwise... we can appropriately wire 4 mux4s to 
+--output the appropriate amount of the barrel-shifted result, and 
+--zero out the rest. I.e., something like:
+
+-- mux4(B(3),B(3),B(3),B(3),S(1 downto 0),D(3))
+-- mux4(B(2),B(2),B(2),0,S(1 downto 0),D(2))
+-- mux4(B(1),B(1),0,0,S(1 downto 0),D(1))
+-- mux4(B(0),0,0,0,S(1 downto 0),D(0))
+
+--Granted, this will have us replicating sll and rol in the 
+--opposite direction for srl and ror. Still don't have sra 
+--figured out though. I'm dog tired though now. It's nearly 
+--four in the morning and I've still got to get to class.
+
+--Who am I kidding. I probably won't. I don't like showing 
+--my face when I'm behind everyone else. Dishonor on me, 
+--Dishonor on my cow...
