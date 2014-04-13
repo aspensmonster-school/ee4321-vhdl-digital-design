9cffb24c1b265e431f78b5c60c7243f2bedfea2c
Preston Maness <aggroskater@gmail.com>
2014-04-13 01:02:57 -0500

    I think I've actually got everything coded now! Now I have to throw it all in WebPACK ISE to find all of the syntax errors and google the solution.

--- Project-4-4bit-ALU/lib/shift_rotate/shift_rotate.vhd
+++ Project-4-4bit-ALU/lib/shift_rotate/shift_rotate.vhd
@@ -42,10 +42,10 @@ magnitude <= B(1 downto 0);
 --but the syntax might be off.
 -------------------------------------------
 
-rol_mux_3: entity work.mux4_bit (A(0), A(1), A(2), A(3), magnitude, rol_R(3))
-rol_mux_2: entity work.mux4_bit (A(3), A(0), A(1), A(2), magnitude, rol_R(2))
-rol_mux_1: entity work.mux4_bit (A(2), A(3), A(0), A(1), magnitude, rol_R(1))
-rol_mux_0: entity work.mux4_bit (A(1), A(2), A(3), A(0), magnitude, rol_R(0))
+rol_mux_3: entity work.mux4_bit (A(0), A(1), A(2), A(3), magnitude, rol_R(3));
+rol_mux_2: entity work.mux4_bit (A(3), A(0), A(1), A(2), magnitude, rol_R(2));
+rol_mux_1: entity work.mux4_bit (A(2), A(3), A(0), A(1), magnitude, rol_R(1));
+rol_mux_0: entity work.mux4_bit (A(1), A(2), A(3), A(0), magnitude, rol_R(0));
 
 -------------------------------------------
 --Rotate Right
@@ -53,10 +53,39 @@ rol_mux_0: entity work.mux4_bit (A(1), A(2), A(3), A(0), magnitude, rol_R(0))
 --just to be on the safe side of things.
 -------------------------------------------
 
-ror_mux_3: entity work.mux4_bit (A(2), A(1), A(0), A(3), magnitude, ror_R(3))
-ror_mux_2: entity work.mux4_bit (A(1), A(0), A(3), A(2), magnitude, ror_R(2))
-ror_mux_1: entity work.mux4_bit (A(0), A(3), A(2), A(1), magnitude, ror_R(1))
-ror_mux_0: entity work.mux4_bit (A(3), A(2), A(1), A(0), magnitude, ror_R(0))
+ror_mux_3: entity work.mux4_bit (A(2), A(1), A(0), A(3), magnitude, ror_R(3));
+ror_mux_2: entity work.mux4_bit (A(1), A(0), A(3), A(2), magnitude, ror_R(2));
+ror_mux_1: entity work.mux4_bit (A(0), A(3), A(2), A(1), magnitude, ror_R(1));
+ror_mux_0: entity work.mux4_bit (A(3), A(2), A(1), A(0), magnitude, ror_R(0));
+
+-------------------------------------------
+--Shift Left Logical
+-------------------------------------------
+
+sll_mux_3: entity work.mux4_bit (A(0), A(1), A(2), A(3), magnitude, sll_R(3));
+sll_mux_2: entity work.mux4_bit (0, A(0), A(1), A(2), magnitude, sll_R(2));
+sll_mux_1: entity work.mux4_bit (0, 0, A(0), A(1), magnitude, sll_R(1));
+sll_mux_0: entity work.mux4_bit (0, 0, 0, A(0), magnitude, sll_R(0));
+
+-------------------------------------------
+--Shift Right Logical
+-------------------------------------------
+
+srl_mux_3: entity work.mux4_bit (0, 0, 0, A(3), magnitude, srl_R(3));
+srl_mux_2: entity work.mux4_bit (0, 0, A(3), A(2), magnitude, srl_R(2));
+srl_mux_1: entity work.mux4_bit (0, A(3), A(2), A(1), magnitude, srl_R(1));
+srl_mux_0: entity work.mux4_bit (A(3), A(2), A(1), A(0), magnitude, srl_R(0));
+
+-------------------------------------------
+--Shift Right Arithmetic
+--same as SRL except instead of hard-coded zeros
+--we fill in the value of A(3)
+-------------------------------------------
+
+sra_mux_3: entity work.mux4_bit (A(3), A(3), A(3), A(3), magnitude, sra_R(3));
+sra_mux_2: entity work.mux4_bit (A(3), A(3), A(3), A(2), magnitude, sra_R(2));
+sra_mux_1: entity work.mux4_bit (A(3), A(3), A(2), A(1), magnitude, sra_R(1));
+sra_mux_0: entity work.mux4_bit (A(3), A(2), A(1), A(0), magnitude, sra_R(0));
 
 -------------------------------------------
 --Output
